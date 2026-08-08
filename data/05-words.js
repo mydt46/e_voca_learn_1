646fe8bd7d8bd15df5b32d94ab34056617648882
mydt <mydt46105@gmail.com>
2026-08-08 18:04:06 +0700

    data 05,06,07

--- data/05-words.js
+++ data/05-words.js
@@ -1,74 +1,74 @@
 const PAGE_WORDS = [
   // --- 5.1 COLORS (US) / COLOURS (UK) ---
   // Hàng 1
-  { answer: "red", left: "6.3", top: "20.7", width: "35" },
-  { answer: "yellow", left: "14.0", top: "20.7", width: "42" },
-  { answer: "green", left: "22.1", top: "20.7", width: "40" },
-  { answer: "dark green", left: "29.0", top: "20.7", width: "62" },
-  { answer: "pale green", left: "37.1", top: "20.7", width: "60" },
+  { answer: "red", left: "9.3", top: "24.7", width: "65" },
+  { answer: "yellow", left: "17.0", top: "24.7", width: "72" },
+  { answer: "green", left: "25.1", top: "24.7", width: "70" },
+  { answer: "dark green", left: "34.0", top: "24.7", width: "92" },
+  { answer: "pale green", left: "42.1", top: "24.7", width: "90" },
 
   // Hàng 2
-  { answer: "blue", left: "6.3", top: "35.9", width: "35" },
-  { answer: "purple", left: "13.8", top: "35.9", width: "45" },
-  { answer: "pink", left: "22.3", top: "35.9", width: "35" },
-  { answer: "white", left: "30.2", top: "35.9", width: "38" },
-  { answer: "gray (US) / grey (UK)", left: "36.4", top: "35.9", width: "75" },
+  { answer: "blue", left: "9.3", top: "39.9", width: "65" },
+  { answer: "purple", left: "16.8", top: "39.9", width: "75" },
+  { answer: "pink", left: "25.3", top: "39.9", width: "65" },
+  { answer: "white", left: "34.2", top: "39.9", width: "68" },
+  { answer: "gray/grey", left: "42.4", top: "38.9", width: "105" },
 
   // Hàng 3
-  { answer: "black", left: "6.2", top: "51.1", width: "38" },
-  { answer: "brown", left: "14.0", top: "51.1", width: "42" },
-  { answer: "orange", left: "21.8", top: "51.1", width: "45" },
-  { answer: "cream", left: "30.0", top: "51.1", width: "42" },
-  { answer: "navy blue", left: "37.1", top: "51.1", width: "60" },
+  { answer: "black", left: "9.2", top: "54.1", width: "68" },
+  { answer: "brown", left: "17.0", top: "54.1", width: "72" },
+  { answer: "orange", left: "24.8", top: "54.1", width: "75" },
+  { answer: "cream", left: "34.0", top: "54.1", width: "72" },
+  { answer: "navy blue", left: "42.1", top: "54.1", width: "90" },
 
   // --- 5.2 MATERIALS ---
   // Hàng 1
-  { answer: "plastic", left: "5.8", top: "75.0", width: "45" },
-  { answer: "wood", left: "14.2", top: "75.0", width: "38" },
-  { answer: "paper", left: "22.0", top: "75.0", width: "42" },
-  { answer: "wool", left: "30.2", top: "75.0", width: "38" },
-  { answer: "leather", left: "37.7", top: "75.0", width: "48" },
+  { answer: "plastic", left: "8.8", top: "78.0", width: "75" },
+  { answer: "wood", left: "17.2", top: "78.0", width: "68" },
+  { answer: "paper", left: "25.0", top: "78.0", width: "72" },
+  { answer: "wool", left: "34.2", top: "78.0", width: "68" },
+  { answer: "leather", left: "42.7", top: "78.0", width: "78" },
 
   // Hàng 2
-  { answer: "metal", left: "6.1", top: "90.2", width: "40" },
-  { answer: "glass", left: "14.2", top: "90.2", width: "38" },
-  { answer: "fabric", left: "22.0", top: "90.2", width: "42" },
-  { answer: "denim", left: "30.0", top: "90.2", width: "42" },
-  { answer: "silk", left: "38.3", top: "90.2", width: "35" },
+  { answer: "metal", left: "9.1", top: "92.2", width: "70" },
+  { answer: "glass", left: "17.2", top: "92.2", width: "68" },
+  { answer: "fabric", left: "25.0", top: "92.2", width: "72" },
+  { answer: "denim", left: "35.0", top: "92.2", width: "72" },
+  { answer: "silk", left: "43.3", top: "92.2", width: "65" },
 
   // --- 5.3 ADJECTIVES ---
   // Hàng 1
-  { answer: "big", left: "55.0", top: "16.1", width: "30" },
-  { answer: "small / little", left: "61.6", top: "16.1", width: "68" },
-  { answer: "wide", left: "71.6", top: "16.1", width: "38" },
-  { answer: "narrow", left: "79.6", top: "16.1", width: "48" },
-  { answer: "deep", left: "88.6", top: "16.1", width: "38" },
+  { answer: "big", left: "58.0", top: "20.1", width: "60" },
+  { answer: "small/little", left: "66.6", top: "20.1", width: "98" },
+  { answer: "wide", left: "75.6", top: "20.1", width: "68" },
+  { answer: "narrow", left: "83.6", top: "20.1", width: "78" },
+  { answer: "deep", left: "93.6", top: "20.1", width: "68" },
 
   // Hàng 2
-  { answer: "high", left: "54.7", top: "33.3", width: "35" },
-  { answer: "low", left: "63.4", top: "33.3", width: "32" },
-  { answer: "heavy", left: "71.4", top: "33.3", width: "42" },
-  { answer: "light", left: "80.1", top: "33.3", width: "38" },
-  { answer: "shallow", left: "88.1", top: "33.3", width: "48" },
+  { answer: "high", left: "57.7", top: "37.3", width: "65" },
+  { answer: "low", left: "66.4", top: "37.3", width: "62" },
+  { answer: "heavy", left: "74.4", top: "37.3", width: "72" },
+  { answer: "light", left: "83.1", top: "37.3", width: "68" },
+  { answer: "shallow", left: "93.1", top: "37.3", width: "78" },
 
   // Hàng 3
-  { answer: "clean", left: "54.6", top: "50.4", width: "38" },
-  { answer: "dirty", left: "63.1", top: "50.4", width: "38" },
-  { answer: "hot", left: "71.9", top: "50.4", width: "32" },
-  { answer: "cold", left: "80.4", top: "50.4", width: "32" },
-  { answer: "long", left: "88.7", top: "50.4", width: "35" },
+  { answer: "clean", left: "57.6", top: "55.4", width: "68" },
+  { answer: "dirty", left: "66.1", top: "55.4", width: "68" },
+  { answer: "hot", left: "74.9", top: "55.4", width: "62" },
+  { answer: "cold", left: "83.4", top: "55.4", width: "62" },
+  { answer: "long", left: "93.7", top: "55.4", width: "65" },
 
   // Hàng 4
-  { answer: "loud", left: "54.7", top: "67.4", width: "35" },
-  { answer: "quiet", left: "63.1", top: "67.4", width: "38" },
-  { answer: "thin", left: "71.9", top: "67.4", width: "32" },
-  { answer: "thick", left: "80.2", top: "67.4", width: "35" },
-  { answer: "short", left: "88.6", top: "67.4", width: "38" },
+  { answer: "loud", left: "57.7", top: "73.4", width: "65" },
+  { answer: "quiet", left: "66.1", top: "73.4", width: "68" },
+  { answer: "thin", left: "74.9", top: "73.4", width: "62" },
+  { answer: "thick", left: "83.2", top: "73.4", width: "65" },
+  { answer: "short", left: "93.6", top: "73.4", width: "68" },
 
   // Hàng 5
-  { answer: "sharp", left: "54.6", top: "84.4", width: "38" },
-  { answer: "tight", left: "63.2", top: "84.4", width: "35" },
-  { answer: "loose", left: "71.6", top: "84.4", width: "38" },
-  { answer: "near", left: "80.2", top: "84.4", width: "35" },
-  { answer: "far", left: "89.0", top: "84.4", width: "30" }
+  { answer: "sharp", left: "58.6", top: "91.4", width: "68" },
+  { answer: "tight", left: "67.2", top: "91.4", width: "65" },
+  { answer: "loose", left: "75.6", top: "91.4", width: "68" },
+  { answer: "near", left: "85.2", top: "91.4", width: "65" },
+  { answer: "far", left: "93.0", top: "91.4", width: "60" },
 ];
--- data/06-words.js
+++ data/06-words.js
@@ -1,79 +1,79 @@
 const PAGE_WORDS = [
   // --- 6.1 OPINION ADJECTIVES (Trang 30) ---
   // Hàng 1
-  { answer: "nice", left: "6.5", top: "24.2", width: "30" },
-  { answer: "fun", left: "14.6", top: "24.2", width: "28" },
-  { answer: "lovely", left: "22.2", top: "24.2", width: "38" },
-  { answer: "delicious", left: "29.5", top: "24.2", width: "52" },
-  { answer: "exciting", left: "37.8", top: "24.2", width: "45" },
+  { answer: "nice", left: "8.5", top: "28.2", width: "60" },
+  { answer: "fun", left: "16.6", top: "28.2", width: "58" },
+  { answer: "lovely", left: "26.2", top: "28.2", width: "68" },
+  { answer: "delicious", left: "33.5", top: "28.2", width: "82" },
+  { answer: "exciting", left: "42.8", top: "28.2", width: "75" },
 
   // Hàng 2
-  { answer: "thrilling", left: "5.8", top: "40.3", width: "45" },
-  { answer: "interesting", left: "13.1", top: "40.3", width: "60" },
-  { answer: "respectable", left: "20.8", top: "40.3", width: "65" },
-  { answer: "special", left: "30.0", top: "40.3", width: "42" },
-  { answer: "graceful", left: "37.7", top: "40.3", width: "48" },
+  { answer: "thrilling", left: "7.8", top: "44.3", width: "75" },
+  { answer: "interesting", left: "17.1", top: "44.3", width: "90" },
+  { answer: "respectable", left: "25.8", top: "44.3", width: "95" },
+  { answer: "special", left: "34.0", top: "44.3", width: "72" },
+  { answer: "graceful", left: "42.7", top: "44.3", width: "78" },
 
   // Hàng 3
-  { answer: "pleasant", left: "5.7", top: "56.4", width: "48" },
-  { answer: "useful", left: "14.1", top: "56.4", width: "40" },
-  { answer: "important", left: "21.3", top: "56.4", width: "55" },
-  { answer: "relaxing", left: "29.7", top: "56.4", width: "48" },
-  { answer: "harmless", left: "37.5", top: "56.4", width: "52" },
+  { answer: "pleasant", left: "7.7", top: "60.4", width: "78" },
+  { answer: "useful", left: "16.1", top: "60.4", width: "70" },
+  { answer: "important", left: "25.3", top: "60.4", width: "85" },
+  { answer: "relaxing", left: "33.7", top: "60.4", width: "78" },
+  { answer: "harmless", left: "42.5", top: "60.4", width: "82" },
 
   // Hàng 4
-  { answer: "frightening", left: "5.1", top: "72.5", width: "60" },
-  { answer: "shocking", left: "13.6", top: "72.5", width: "50" },
-  { answer: "irritating", left: "21.6", top: "72.5", width: "50" },
-  { answer: "annoying", left: "29.6", top: "72.5", width: "50" },
-  { answer: "horrible", left: "37.8", top: "72.5", width: "45" },
+  { answer: "frightening", left: "9.1", top: "76.5", width: "90" },
+  { answer: "shocking", left: "17.6", top: "76.5", width: "80" },
+  { answer: "irritating", left: "25.6", top: "76.5", width: "80" },
+  { answer: "annoying", left: "34.6", top: "76.5", width: "80" },
+  { answer: "horrible", left: "42.8", top: "76.5", width: "75" },
 
   // Hàng 5
-  { answer: "nasty", left: "6.2", top: "88.6", width: "38" },
-  { answer: "old-fashioned", left: "12.4", top: "88.6", width: "75" },
-  { answer: "unpleasant", left: "21.0", top: "88.6", width: "62" },
-  { answer: "disastrous", left: "29.2", top: "88.6", width: "58" },
-  { answer: "useless", left: "37.8", top: "88.6", width: "45" },
+  { answer: "nasty", left: "8.2", top: "92.6", width: "68" },
+  { answer: "old-fashioned", left: "16.4", top: "92.6", width: "105" },
+  { answer: "unpleasant", left: "26.0", top: "92.6", width: "92" },
+  { answer: "disastrous", left: "34.2", top: "92.6", width: "88" },
+  { answer: "useless", left: "42.8", top: "92.6", width: "75" },
 
   // --- 6.1 OPINION ADJECTIVES (Trang 31 - bên trái) ---
   // Hàng 1
-  { answer: "stunning", left: "52.9", top: "24.2", width: "52" },
-  { answer: "magnificent", left: "60.6", top: "24.2", width: "68" },
+  { answer: "stunning", left: "56.9", top: "28.2", width: "82" },
+  { answer: "magnificent", left: "66.0", top: "28.2", width: "98" },
 
   // Hàng 2
-  { answer: "pretty", left: "53.6", top: "40.3", width: "38" },
-  { answer: "beautiful", left: "61.4", top: "40.3", width: "52" },
+  { answer: "pretty", left: "57.6", top: "44.3", width: "68" },
+  { answer: "beautiful", left: "66.0", top: "44.3", width: "82" },
 
   // Hàng 3
-  { answer: "disgusting", left: "52.6", top: "56.4", width: "58" },
-  { answer: "boring", left: "62.1", top: "56.4", width: "38" },
+  { answer: "disgusting", left: "58.6", top: "60.4", width: "88" },
+  { answer: "boring", left: "66.1", top: "60.4", width: "68" },
 
   // Hàng 4
-  { answer: "ugly", left: "54.0", top: "72.5", width: "30" },
-  { answer: "strange / odd", left: "60.3", top: "72.5", width: "75" },
+  { answer: "ugly", left: "58.0", top: "76.5", width: "60" },
+  { answer: "strange/odd", left: "66.3", top: "76.5", width: "105" },
 
   // Hàng 5
-  { answer: "confusing", left: "52.6", top: "88.6", width: "58" },
-  { answer: "tiring", left: "62.1", top: "88.6", width: "38" },
+  { answer: "confusing", left: "57.6", top: "92.6", width: "88" },
+  { answer: "tiring", left: "66.1", top: "92.6", width: "68" },
 
   // --- 6.2 GOOD (Trang 31 - trên bên phải) ---
-  { answer: "remarkable", left: "71.0", top: "15.4", width: "65" },
-  { answer: "extraordinary", left: "77.6", top: "15.4", width: "75" },
-  { answer: "breathtaking", left: "84.9", top: "15.4", width: "72" },
+  { answer: "remarkable", left: "76.0", top: "19.4", width: "90" },
+  { answer: "extraordinary", left: "83.6", top: "19.4", width: "105" },
+  { answer: "breathtaking", left: "91.9", top: "19.4", width: "102" },
 
-  { answer: "outstanding", left: "68.8", top: "28.0", width: "68" },
-  { answer: "wonderful", left: "88.4", top: "28.0", width: "60" },
-  { answer: "amazing", left: "68.8", top: "39.9", width: "52" },
-  { answer: "fantastic", left: "88.9", top: "39.9", width: "55" },
+  { answer: "outstanding", left: "73.8", top: "32.0", width: "98" },
+  { answer: "wonderful", left: "94.4", top: "32.0", width: "90" },
+  { answer: "amazing", left: "73.8", top: "43.9", width: "82" },
+  { answer: "fantastic", left: "94.9", top: "43.9", width: "85" },
 
-  { answer: "great", left: "70.4", top: "49.4", width: "35" },
-  { answer: "superb", left: "76.1", top: "49.4", width: "45" },
-  { answer: "brilliant", left: "82.4", top: "49.4", width: "48" },
-  { answer: "incredible", left: "88.6", top: "49.4", width: "58" },
+  { answer: "great", left: "74.4", top: "53.4", width: "65" },
+  { answer: "superb", left: "81.1", top: "53.4", width: "75" },
+  { answer: "brilliant", left: "88.0", top: "53.4", width: "78" },
+  { answer: "incredible", left: "94.6", top: "53.4", width: "88" },
 
   // --- 6.3 BAD (Trang 31 - dưới bên phải) ---
-  { answer: "awful", left: "70.6", top: "72.9", width: "38" },
-  { answer: "mediocre", left: "88.8", top: "72.9", width: "52" },
-  { answer: "terrible", left: "74.6", top: "88.6", width: "48" },
-  { answer: "disappointing", left: "82.8", top: "88.6", width: "75" }
+  { answer: "awful", left: "75.6", top: "76.9", width: "68" },
+  { answer: "mediocre", left: "93.8", top: "76.9", width: "82" },
+  { answer: "terrible", left: "78.6", top: "92.6", width: "78" },
+  { answer: "disappointing", left: "89.8", top: "92.6", width: "105" },
 ];
