ea1043a8aadfca09fc15986fafd63043ee2f8778
mydt <mydt46105@gmail.com>
2026-08-14 02:45:04 +0700

    add 14,15

--- data/15-words.js
+++ data/15-words.js
@@ -1,77 +1,137 @@
 const PAGE_WORDS = [
-  // --- PAGE 62: 14.1 PARTS OF THE BODY (Cột icon bên trái) ---
-  { answer: "ear", left: "7.2", top: "23.1", width: "30" },
-  { answer: "eye", left: "7.2", top: "30.1", width: "30" },
-  { answer: "eyebrow", left: "6.2", top: "37.1", width: "45" },
-  { answer: "eyelashes", left: "5.8", top: "44.1", width: "52" },
-  { answer: "nose", left: "7.1", top: "51.1", width: "32" },
-  { answer: "nostrils", left: "6.2", top: "58.1", width: "45" },
-  { answer: "mouth", left: "6.5", top: "65.1", width: "40" },
-  { answer: "lips", left: "7.2", top: "72.1", width: "30" },
-  { answer: "teeth", left: "6.8", top: "79.1", width: "35" },
-  { answer: "tooth", left: "6.8", top: "86.1", width: "35" },
+  // --- PAGE 66: 15.1 CLOTHES (Sơ đồ áo sơ mi) ---
+  { answer: "cuff", left: "9.8", top: "16.4", width: "60" },
+  { answer: "collar", left: "16.0", top: "16.4", width: "65" },
+  { answer: "sleeve", left: "8.8", top: "34.0", width: "68" },
+  { answer: "button", left: "19.5", top: "34.0", width: "68" },
+  { answer: "shirt", left: "14.0", top: "42.3", width: "62" },
 
-  // --- PAGE 62: 14.1 PARTS OF THE BODY (Sơ đồ cơ thể) ---
-  // Nhãn bên trái sơ đồ
-  { answer: "head", left: "20.8", top: "23.1", width: "32" },
-  { answer: "cheek", left: "20.2", top: "32.0", width: "38" },
-  { answer: "neck", left: "20.8", top: "42.0", width: "32" },
-  { answer: "shoulder", left: "18.8", top: "52.0", width: "50" },
-  { answer: "elbow", left: "19.8", top: "61.0", width: "38" },
-  { answer: "arm", left: "20.8", top: "70.0", width: "30" },
-  { answer: "hand", left: "20.2", top: "78.5", width: "35" },
-  { answer: "thigh", left: "20.5", top: "85.5", width: "35" },
-  { answer: "leg", left: "21.5", top: "91.0", width: "28" },
-  { answer: "ankle", left: "20.5", top: "94.5", width: "35" },
-  { answer: "foot", left: "20.8", top: "97.5", width: "30" },
-
-  // Nhãn bên phải sơ đồ
-  { answer: "forehead", left: "38.0", top: "20.5", width: "50" },
-  { answer: "face", left: "39.2", top: "30.0", width: "30" },
-  { answer: "chin", left: "39.0", top: "40.0", width: "28" },
-  { answer: "chest", left: "38.8", top: "49.0", width: "32" },
-  { answer: "waist", left: "38.6", top: "58.0", width: "32" },
-  { answer: "stomach", left: "37.8", top: "67.0", width: "48" },
-  { answer: "hip", left: "39.2", top: "75.0", width: "28" },
-  { answer: "thumb", left: "38.2", top: "82.5", width: "38" },
-  { answer: "finger", left: "38.2", top: "88.0", width: "38" },
-  { answer: "knee", left: "38.8", top: "92.5", width: "32" },
-  { answer: "shin", left: "39.0", top: "96.0", width: "28" },
-  { answer: "toe", left: "39.2", top: "98.5", width: "28" },
-
-  // --- PAGE 63: 14.2 VERBS (Động cơ thể) ---
+  // --- PAGE 66: 15.1 CLOTHES (Lưới bên phải sơ đồ sơ mi) ---
   // Hàng 1
-  { answer: "to smile", left: "54.0", top: "23.1", width: "48" },
-  { answer: "to frown", left: "62.2", top: "23.1", width: "48" },
-  { answer: "to grin", left: "71.2", top: "23.1", width: "40" },
-  { answer: "to laugh", left: "79.2", top: "23.1", width: "48" },
-  { answer: "to cry", left: "88.2", top: "23.1", width: "38" },
+  { answer: "t-shirt", left: "25.3", top: "25.1", width: "68" },
+  { answer: "blouse", left: "34.3", top: "25.1", width: "68" },
+  { answer: "suit", left: "42.6", top: "25.1", width: "60" },
 
   // Hàng 2
-  { answer: "to yawn", left: "53.8", top: "39.4", width: "48" },
-  { answer: "to cough", left: "62.0", top: "39.4", width: "50" },
-  { answer: "to sigh", left: "71.0", top: "39.4", width: "38" },
-  { answer: "to lick", left: "79.6", top: "39.4", width: "38" },
-  { answer: "to suck", left: "88.0", top: "39.4", width: "40" },
+  { answer: "uniform", left: "25.6", top: "42.0", width: "78" },
+  { answer: "dress", left: "34.5", top: "42.0", width: "65" },
+  { answer: "skirt", left: "42.4", top: "42.0", width: "62" },
 
+  // --- PAGE 66: 15.1 CLOTHES (Lưới 5 cột) ---
   // Hàng 3
-  { answer: "to breathe", left: "53.2", top: "55.7", width: "58" },
-  { answer: "to hold your breath", left: "60.5", top: "55.7", width: "92" },
-  { answer: "to wink", left: "71.0", top: "55.7", width: "40" },
-  { answer: "to blink", left: "79.4", top: "55.7", width: "42" },
-  { answer: "to blush", left: "87.8", top: "55.7", width: "42" },
+  {
+    answer: "pants/trousers",
+    left: "8.5",
+    top: "58.0",
+    width: "115",
+  },
+  { answer: "jeans", left: "17.0", top: "58.7", width: "65" },
+  { answer: "jacket", left: "25.1", top: "58.7", width: "68" },
+  { answer: "raincoat", left: "34.0", top: "58.7", width: "78" },
+  { answer: "socks", left: "43.2", top: "58.7", width: "65" },
 
   // Hàng 4
-  { answer: "to sweat / to perspire", left: "51.8", top: "71.8", width: "100" },
-  { answer: "to shiver", left: "62.2", top: "71.8", width: "48" },
-  { answer: "to snore", left: "70.8", top: "71.8", width: "45" },
-  { answer: "to sneeze", left: "78.8", top: "71.8", width: "52" },
-  { answer: "to wave", left: "87.8", top: "71.8", width: "42" },
+  { answer: "shorts", left: "8.8", top: "74.8", width: "68" },
+  { answer: "tie", left: "17.6", top: "74.8", width: "58" },
+  {
+    answer: "pajamas/pyjamas",
+    left: "25.8",
+    top: "74.8",
+    width: "110",
+  },
+  {
+    answer: "zipper/zip",
+    left: "31.3",
+    top: "67.4",
+    width: "110",
+  },
+  { answer: "hood", left: "45.8", top: "66.8", width: "62" },
+  { answer: "pocket", left: "32.1", top: "79.8", width: "72" },
+  { answer: "coat", left: "38.8", top: "91.1", width: "62" },
 
   // Hàng 5
-  { answer: "to clap", left: "54.2", top: "88.1", width: "42" },
-  { answer: "to shrug", left: "62.2", top: "88.1", width: "48" },
-  { answer: "to bow", left: "71.2", top: "88.1", width: "38" },
-  { answer: "to nod", left: "79.6", top: "88.1", width: "38" },
-  { answer: "to shake your head", left: "86.0", top: "88.1", width: "82" }
+  {
+    answer: "sweater/jumper",
+    left: "9.1",
+    top: "91.1",
+    width: "110",
+  },
+  { answer: "bra", left: "17.6", top: "92.1", width: "58" },
+  {
+    answer: "panties/knickers",
+    left: "25.9",
+    top: "91.1",
+    width: "110",
+  },
+
+  // --- PAGE 67: 15.1 CLOTHES (Cột trái trang 67) ---
+  {
+    answer: "bathrobe/ dressing gown",
+    left: "58.2",
+    top: "25.3",
+    width: "115",
+  },
+  { answer: "boxer shorts", left: "59.0", top: "42.4", width: "98" },
+  {
+    answer: "jockey shorts/ pants",
+    left: "59.0",
+    top: "58.7",
+    width: "120",
+  },
+  {
+    answer: "pantyhose/tights",
+    left: "59.2",
+    top: "75.4",
+    width: "115",
+  },
+  { answer: "leggings", left: "59.0", top: "91.8", width: "78" },
+
+  // --- PAGE 67: 15.2 VERBS (Góc trên bên phải) ---
+  // Hàng 1
+  { answer: "to wear", left: "69.5", top: "25.0", width: "72" },
+  { answer: "to suit (someone)", left: "78.2", top: "23.5", width: "95" },
+  { answer: "to fit", left: "85.6", top: "25.0", width: "60" },
+  { answer: "to put on", left: "93.0", top: "25.0", width: "82" },
+
+  // Hàng 2
+  { answer: "to take off", left: "69.5", top: "36.5", width: "90" },
+  { answer: "to do up / fasten", left: "77.6", top: "36.0", width: "90" },
+  { answer: "to undo / unfasten", left: "85.2", top: "36.0", width: "100" },
+  {
+    answer: "to change / to get changed",
+    left: "93.0",
+    top: "36.0",
+    width: "108",
+  },
+
+  // --- PAGE 67: 15.3 DESCRIBING CLOTHES AND STYLES (Góc dưới bên phải) ---
+  // Hàng 1
+  { answer: "leather", left: "70.1", top: "61.1", width: "75" },
+  { answer: "cotton", left: "81.5", top: "61.1", width: "72" },
+  {
+    answer: "woolen/woollen",
+    left: "91.5",
+    top: "61.0",
+    width: "110",
+  },
+
+  // Hàng 2
+  {
+    answer: "polka dot/ spotted",
+    left: "71.8",
+    top: "76.0",
+    width: "115",
+  },
+  { answer: "striped", left: "81.2", top: "76.0", width: "75" },
+  {
+    answer: "checkered/checked",
+    left: "91.2",
+    top: "76.0",
+    width: "115",
+  },
+
+  // Hàng 3
+  { answer: "plain", left: "70.8", top: "91.5", width: "65" },
+  { answer: "silk", left: "81.0", top: "91.5", width: "62" },
+  { answer: "denim", left: "92.0", top: "91.5", width: "68" },
 ];
